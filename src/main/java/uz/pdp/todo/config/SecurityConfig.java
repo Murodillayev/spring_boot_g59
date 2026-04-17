7f85d388f20abce729f6fec1a41798df244b5a19
muhammad <olmospro1978@gmail.com>
2026-04-17 10:25:43 +0500

    teste

---
+++ src/main/java/uz/pdp/todo/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package uz.pdp.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth
+//                        .requestMatchers("/todo")
+//                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+        );
+
+        http.sessionManagement(
+                securitySessionManagementConfigurer ->
+                        securitySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("123")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
