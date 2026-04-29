c0680d4fda14a097afb4184f3f2a8d78e7b6a39b
muhammad <olmospro1978@gmail.com>
2026-04-29 11:45:12 +0500

    teste

--- src/main/java/uz/pdp/todo/AppConfig.java
+++ src/main/java/uz/pdp/todo/AppConfig.java
@@ -3,6 +3,7 @@ package uz.pdp.todo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +11,11 @@ public class AppConfig {
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+
+    @Bean
+    public WebClient getWebClient() {
+        return WebClient.create();
     }
 }
--- src/main/java/uz/pdp/todo/PostApiService.java
+++ src/main/java/uz/pdp/todo/PostApiService.java
@@ -1,31 +1,20 @@
 package uz.pdp.todo;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(url = "https://jsonplaceholder.typicode.com/posts", name = "jsonplaceholder")
 public interface PostApiService {
 
-    @GetMapping
     List<Post> getAllByGetForObject();
 
-    @GetMapping
     ResponseEntity<List<Post>> getAllByGetForEntity();
 
-    @GetMapping("/{id}")
-    Post getByGetForObject(@PathVariable String id);
+    Post getByGetForObject(String id);
 
-    @GetMapping("/{id}")
-    ResponseEntity<Post> getByGetForEntity(@PathVariable String id);
+    ResponseEntity<Post> getByGetForEntity(String id);
 
-    @PostMapping
-    Post postForObject(@RequestBody PostCreateDto dto);
+    Post postForObject(PostCreateDto dto);
 
 }
 
